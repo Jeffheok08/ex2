a7a2874a517340b639599f1c701ca6e70a3f1741
Narit Suttipong <totly000@hotmail.com>
2019-07-13 14:13:51 +0700

    เพิ่มหน้าtest

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const port = 3000
 app.get('/', (req, res) => res.send('ชื่อเรา'))
 app.get('/about', (req, res) => res.send('เกี่ยวกับ'))
 app.get('/contact', (req, res) => res.send('ติดต่อเรา'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+}
+
+)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
